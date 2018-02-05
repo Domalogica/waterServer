f5511a8feb17cb7d23158fba5e4e7d96fafbb1bf
WittiLion <lioknuckles@gmail.com>
2018-02-05 11:27:57 +0300

    New_DB

--- db_menedger.py
+++ db_menedger.py
@@ -3,38 +3,91 @@
 """
 
 import time
+import pymysql
 
-#тестовые данные
+# тестовые данные
 db_wm = [{'wm': 0, 'total_paid': 0, 'water_praise': 400, 'max_volume_tank': 120000, 'last_time': 0},
          {'wm': 1, 'total_paid': 0, 'water_praise': 400, 'max_volume_tank': 120000, 'last_time': 0}]
 
-#тестовые данные
+# тестовые данные
 db_usr = [{'telegram': 23, 'score': 100},
           {'telegram': 24, 'score': 100}]
 
+class MysqlPython(object):
 
-def get_last_time(wm):
-    return db_wm[wm]['last_time']
+    _instance   = None
+    _host       = None
+    _user       = None
+    _password   = None
+    _database   = None
+    _session    = None
+    _connection = None
 
-
-def get_config_wm(wm):
-    try:
-        return db_wm[wm]
-    except IndexError:
-        return {'ok': False}
-
-
-def update_last_time(wm):
-    db_wm[wm].update({'last_time': int(time.time())})
-
-
-def set_config_wm(wm, data):
-    try:
-        db_wm[wm].update(data)
-        return {'ok': True}
-    except IndexError:
-        return {'ok': False}
+    def __init__(self, host='localhost', user='root', password='', database='', charset='utf8'):
+        self.__host     = host
+        self.__user     = user
+        self.__password = password
+        self.__database = database
 
 
-def get_user_score(telegram):
-    return db_usr[telegram]
+    def __open(self):
+        try:
+            cnx = pymysql.connect(self.__host, self.__user, self.__password, self.__database)
+            self._connection = cnx
+            self._session    = cnx.cursor()
+        except pymysql.Error as e:
+            print("Error %d: %s" % (e.args[0],e.args[1]))
+
+    def __close(self):
+        self._session.close()
+        self._connection.close()
+
+
+
+
+
+    def get_last_time(wm):
+        return db_wm[wm]['last_time']
+
+
+    def get_config_wm(wm):
+        try:
+            return db_wm[wm]
+        except IndexError:
+            return {'ok': False}
+
+
+    def update_last_time(wm):
+        db_wm[wm].update({'last_time': int(time.time())})
+
+
+    def set_config_wm(wm, data):
+        try:
+            db_wm[wm].update(data)
+            return {'ok': True}
+        except IndexError:
+            return {'ok': False}
+
+
+    def get_user_score(telegram):
+        return db_usr[telegram]
+
+
+    def insert_session(self, param):
+
+        query = "INSERT INTO %s " % table
+        keys = param.keys()
+        values = tuple(param.values())
+        query += "(" + ",".join(["`%s`"] * len(keys)) % tuple(keys) + ") VALUES (" + ",".join(["%s"] * len(values)) + ")"
+
+        self.__open()
+
+        self.__session.execute(query, values)
+        self.__connection.commit()
+        self.__close()
+        # return self.__session.lastrowid
+
+        return {'session': session}
+
+
+connect_mysql = MysqlPython('127.0.0.1', 'root', '7087', 'WB')
